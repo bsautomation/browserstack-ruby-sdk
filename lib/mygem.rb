cdf7b134616e0d4881f905da7ea4f41abc921108
Saurav Das <saurav.d@browserstack.com>
2022-11-01 18:14:22 +0530

    chore: Hit endpoints

--- lib/mygem.rb
+++ lib/mygem.rb
@@ -1,13 +1,20 @@
 require_relative 'runner'
-require 'pry'
+require 'uri'
+require 'net/http'
 
 class Mygem
+  API_ENDPOINT = 'testops-collector-stag.us-east-1.elasticbeanstalk.com'
+  BUILDS = '/api/v1/builds'
+  
+
   def custom_formatter(config, ast)
     @runner = Runner.new
     @ast_lookup = ast
-    %i[test_case_started test_case_finished test_step_started test_step_finished test_run_finished].each do |event_name|
+    %i[test_run_started test_case_started test_case_finished test_step_started test_step_finished test_run_finished].each do |event_name|
       config.on_event event_name do |event|
         case event_name
+        when :test_run_started
+          on_test_run_started(event)
         when :test_case_started
           on_test_case_started(event)
         when :test_case_finished
@@ -25,31 +32,35 @@ class Mygem
     end
   end
 
+  def on_test_run_started(event)
+    puts "Mygem - inside on_test_run_started - #{event}"
+    request_data = create_build_response(current_time)
+    res = launchTestSession(request_data)
+    res = JSON.parse(res)
+    @build_hashed_id = res['build_hashed_id']
+    @jwt_token = res['jwt']
+  end
+
   def on_test_case_started(event)
-    puts "LOCAL GEM - inside on_test_case_started - #{event}"
-    start_time = current_time
-    test_case = event.test_case
-    scenario = @ast_lookup.scenario_source(test_case)
-    @scenario_name = get_scenario_name(scenario)
-    @tags_array = get_tags_array(test_case.tags)
-    response_data = create_build_response(start_time)
-    make_request(response_data)
+    puts "Mygem - inside on_test_case_started - #{event}"
   end
 
   def on_test_case_finished(event)
-    puts "LOCAL GEM - inside on_test_case_finished - #{event}"
+    puts "Mygem - inside on_test_case_finished - #{event}"
   end
 
   def on_test_step_started(event)
-    puts "LOCAL GEM - inside test_step_started  - #{event}"
+    puts "Mygem - inside test_step_started  - #{event}"
   end
 
   def on_test_step_finished(event)
-    puts "LOCAL GEM - test_step_finished - #{event}"
+    puts "Mygem - test_step_finished - #{event}"
   end
 
   def on_test_run_finished(event)
-    puts "LOCAL GEM - inside test_run_finished - #{event}"
+    puts "Mygem - inside test_run_finished - #{event}"
+    request_data = create_stop_response(current_time)
+    stopBuildUpstream(request_data)
   end
 
   def get_tags_array(tags)
@@ -70,7 +81,7 @@ class Mygem
       'name' => @runner.get_build_name,
       'description' => @runner.get_description,
       'start_time' => time,
-      'tags' => @tags_array,
+      'tags' => @runner.get_build_tag,
       'host_info' => @runner.get_host_machine_info,
       'ci_info' => @runner.get_ci_info,
       'failed_tests_rerun' => @runner.get_failed_test_rerun,
@@ -78,21 +89,41 @@ class Mygem
     }
   end
 
+  def launchTestSession(data)
+    data = data.to_json
+    endpoint = "http://#{API_ENDPOINT}#{BUILDS}"
+    auth = "#{@runner.get_username}:#{@runner.get_access_key}"
+    headers = 'Content-Type: application/json'
+    res = `curl -X POST '#{endpoint}' -u #{auth} -H '#{headers}' -d '#{data}'`
+    res
+  end
+
   # /stop api
-  def create_stop_response()
+  def create_stop_response(time)
+    { 'stop_time' => time }
+  end
+
+  def stopBuildUpstream(data)
+    data = data.to_s
+    endpoint = "http://#{API_ENDPOINT}#{BUILDS}/#{@build_hashed_id}/stop"
+    headers = {
+      'Authorization' => "Bearer #{@jwt_token}",
+      'Content-Type' => 'application/json',
+      'X-BSTACK-TESTOPS': 'true'
+    }
+    uri = URI(endpoint)
+    req = Net::HTTP::Put.new(uri.path, initheader = headers)
+    req.body = data
+    response = Net::HTTP.new(uri.host, uri.port).start {|http| http.request(req) }
+    puts response.code 
+    p '/stop build failed' if response.code != '200'
   end
 
   # /events
   def create_events_response()
   end
 
-  def make_request(data)
-    data = data.to_json
-    request = "curl -d '#{data}' -H 'Content-Type: application/json' -X POST http://localhost:4000/time --silent"
-    `#{request}`
-  end
-
   def current_time
-    Time.now
+    Time.now.to_s
   end
 end
